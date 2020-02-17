70a8d3779c81fd3321a903b0d67bfb3a24488b11
samuelmorenov <UO266321@uniovi.es>
2020-02-17 21:04:51 +0100

    SDI-session3-Ejercicio-complementario3

--- src/main/java/com/uniovi/controllers/MarksController.java
+++ src/main/java/com/uniovi/controllers/MarksController.java
@@ -3,6 +3,7 @@ package com.uniovi.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.uniovi.entities.Mark;
 import com.uniovi.services.MarksService;
 import com.uniovi.services.UsersService;
+import com.uniovi.validators.AddMarkValidator;
 
 @Controller
 public class MarksController {
@@ -20,6 +22,9 @@ public class MarksController {
 
 	@Autowired
 	private UsersService usersService;
+
+	@Autowired
+	private AddMarkValidator addMarkValidator;
 
 	@RequestMapping("/mark/list/update")
 	public String updateList(Model model) {
@@ -34,7 +39,11 @@ public class MarksController {
 	}
 
 	@RequestMapping(value = "/mark/add", method = RequestMethod.POST)
-	public String setMark(@ModelAttribute Mark mark) {
+	public String setMark(@ModelAttribute Mark mark, BindingResult result, Model model) {
+		addMarkValidator.validate(mark, result);
+		if (result.hasErrors()) {
+			return "mark/add";
+		}
 		marksService.addMark(mark);
 
 		return "redirect:/mark/list";
