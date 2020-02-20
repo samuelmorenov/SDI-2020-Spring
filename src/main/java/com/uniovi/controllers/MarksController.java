f927b641e98a0f9e3492dcc21932834ffbc0550f
samuelmorenov <UO266321@uniovi.es>
2020-02-20 18:29:10 +0100

    SDI-SpringBoot-Sesión-4.1-Sesiones

--- src/main/java/com/uniovi/controllers/MarksController.java
+++ src/main/java/com/uniovi/controllers/MarksController.java
@@ -1,5 +1,10 @@
 package com.uniovi.controllers;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,6 +21,9 @@ import com.uniovi.validators.AddMarkValidator;
 
 @Controller
 public class MarksController {
+
+	@Autowired
+	private HttpSession httpSession;
 
 	@Autowired // Inyectar el servicio
 	private MarksService marksService;
@@ -34,6 +42,12 @@ public class MarksController {
 
 	@RequestMapping("/mark/list")
 	public String getList(Model model) {
+		/*
+		 * Set<Mark> consultedList = (Set<Mark>)
+		 * httpSession.getAttribute("consultedList"); if (consultedList == null) {
+		 * consultedList = new HashSet<Mark>(); } model.addAttribute("consultedList",
+		 * consultedList);
+		 */
 		model.addAttribute("markList", marksService.getMarks());
 		return "mark/list";
 	}
